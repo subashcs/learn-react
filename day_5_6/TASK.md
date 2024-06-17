8d2b0c5f7c8e26e3331706faee08210c4224fa8d
Subash Chandra Sapkota <scs@logpoint.com>
2024-06-17 14:47:19 +0545

    Update day 6 task

---
+++ day_5_6/TASK.md
@@ -0,0 +1,8 @@
+# TASK
+Task for this session.
+
+1. Run the `inventory-api` api using command `npm run start`
+2. Run the `store-management` frontend using command `npm run dev`
+3. Analyze the frontend project structure
+4. Complete the useEffect hook logic inside [ProductList.jsx](./store-management/src/components/ProductList.jsx). You will update the logic to fetch products from our API and set the output to `products` state variable.
+   
--- day_5_6/store-management/src/components/ProductList.jsx
+++ day_5_6/store-management/src/components/ProductList.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { Table, Button } from "react-bootstrap";
 
-const products = [
+const initialProducts = [
   { id: 1, name: "Product 1", price: 100, quantity: 10 },
   { id: 2, name: "Product 2", price: 200, quantity: 5 },
   // Add more products as needed
@@ -10,9 +11,25 @@ const products = [
 function ProductList() {
   // list products by fetching from api by replacing constant
   // moving the data fetching logic to a custom hook
+  const [products, setProducts] = useState(initialProducts);
+
+  // This function executes after component mounts on the dom
   useEffect(() => {
-    console.log("component mount");
-    fetch("http://localhost:3000/products");
+    // Task 1: Complete this effect hook
+    // set the fetched data response correctly to products state variable
+    // using the useEffect hook
+    // Hint:
+    // 1. Log the response to verify and then set the response
+    // to products state
+    // 2. you can use .then method to get the response from fetch request
+
+    fetch("http://localhost:3000/products").then((response) => {
+      console.log("got api response", response);
+      // Do something here with the response and set it to products
+      // YOUR CODE GOES HERE
+
+      // *******
+    });
   }, []);
 
   return (
