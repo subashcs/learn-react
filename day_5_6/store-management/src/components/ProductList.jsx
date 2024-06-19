4b13ddc25f7c554f1c5cba73ae43e6fd20c5fab5
Subash Chandra Sapkota <scs@logpoint.com>
2024-06-19 09:42:29 +0545

    Add custom hook

--- day_5_6/store-management/src/components/ProductList.jsx
+++ day_5_6/store-management/src/components/ProductList.jsx
@@ -1,35 +1,8 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import { Table, Button } from "react-bootstrap";
-
-const initialProducts = [];
+import useProducts from "../hooks/useProducts";
 
 function ProductList() {
-  // list products by fetching from api by replacing constant
-  // moving the data fetching logic to a custom hook
-  const [products, setProducts] = useState(initialProducts);
-
-  const getData = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/products");
-      const result = await response.json();
-      setProducts(result);
-    } catch (err) {
-      console.error(">>>>>>Error:", err);
-    }
-  };
-
-  // This function executes after component mounts on the dom
-  useEffect(() => {
-    // Task 1: Complete this effect hook
-    // set the fetched data response correctly to products state variable
-    // using the useEffect hook
-    // Hint:
-    // 1. Log the response to verify and then set the response
-    // to products state
-    // 2. you can use .then method to get the response from fetch request
-    getData();
-  }, []);
+  const products = useProducts();
 
   return (
     <Table striped bordered hover>
